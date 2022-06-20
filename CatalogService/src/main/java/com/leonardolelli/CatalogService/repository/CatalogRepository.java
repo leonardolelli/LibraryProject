3befc61ef37c7ac6b58a1be63740399302ff5010
Lelli Leonardo <md2leolelli@icc.crifnet.com>
2022-06-20 12:40:54 +0200

    typo fix

--- CatalogService/src/main/java/com/leonardolelli/CatalogService/repository/CatalogRepository.java
+++ CatalogService/src/main/java/com/leonardolelli/CatalogService/repository/CatalogRepository.java
@@ -10,11 +10,11 @@ import com.leonardolelli.CatalogService.model.Book.Genre;
 
 public interface CatalogRepository extends MongoRepository<Book, String> {
 	@Query("{genre : ?0}")
-	List<Book> findAllByGenre(Genre horror);
+	List<Book> findAllByGenre(Genre genre);
 
 	@Query("{author : ?0}")
 	List<Book> findAllByAuthor(String author);
 
 	@Query("{$and:[{genre: ?0},{author: ?1}]}")
-	List<Book> findAllByGenreAndAuthor(Genre horror, String author);
+	List<Book> findAllByGenreAndAuthor(Genre genre, String author);
 }
