afe752ebc20cd4b9a8a87f5daf816cf4adab4123
Lelli Leonardo <md2leolelli@icc.crifnet.com>
2022-06-21 17:48:36 +0200

    added controller advice

---
+++ RentalService/src/main/java/com/leonardolelli/RentalService/controller/RentControllerAdvice.java
@@ -0,0 +1,42 @@
+package com.leonardolelli.RentalService.controller;
+
+import java.sql.SQLException;
+import java.sql.SQLTransientConnectionException;
+import java.util.NoSuchElementException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.leonardolelli.RentalService.exception.BookAlreadyReturnedException;
+import com.leonardolelli.RentalService.exception.BookNotAvailableException;
+
+@RestControllerAdvice
+public class RentControllerAdvice {
+
+    @ExceptionHandler(SQLTransientConnectionException.class)
+    @ResponseStatus(value = HttpStatus.SERVICE_UNAVAILABLE, reason = "DB service not available")
+    void SqlTransientHandle(SQLTransientConnectionException exc) {
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Element not present")
+    void NoSuchElementHandle(NoSuchElementException exc) {
+    }
+
+    @ExceptionHandler(SQLException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Issues with your operation")
+    void SQLHandle(SQLException exc) {
+    }
+
+    @ExceptionHandler(BookAlreadyReturnedException.class)
+    @ResponseStatus(value = HttpStatus.CONFLICT, reason = "The book was already returned")
+    void GenreNotValidHandle(BookAlreadyReturnedException exc) {
+    }
+
+    @ExceptionHandler(BookNotAvailableException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Book not available")
+    void BookNotAvailableExceptionHandle(BookNotAvailableException exc) {
+    }
+}
